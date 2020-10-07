d51b88a57821490442297d667f1b38548eb29974
drahoslav.madar@aliter.com <drahoslav.madar@aliter.com>
2020-10-07 13:01:20 +0200

    rendering sprite on random canvas spot in loop

--- game.js
+++ game.js
@@ -2,23 +2,55 @@ class Game {
   constructor() {
     this.canvas = document.getElementById("game");
     this.context = this.canvas.getContext("2d");
-
     this.spriteImage = new Image();
-    this.spriteImage.src = "assets/DMA.jpg";
-
+    this.spriteImage.src = "assets/magic.png";
+    this.sprites = [];
     const game = this;
 
     this.spriteImage.onload = (event) => {
-      const options = {
-        context: game.context,
-        width: this.width,
-        height: this.height,
-        image: this.spriteImage,
-      };
-      game.sprite = new Sprite(options);
-      game.sprite.render();
+      game.lastRefreshTime = Date.now();
+      game.spawn();
+      game.refresh();
     };
   }
+
+  spawn = () => {
+    const options = {
+      context: this.context,
+      x: Math.random() * this.canvas.width,
+      y: Math.random() * this.canvas.height,
+      width: this.spriteImage.width,
+      height: this.spriteImage.height,
+      image: this.spriteImage,
+    };
+    const sprite = new Sprite(options);
+    this.sprites.push(sprite);
+    this.sinceLastSpawn = 0;
+  };
+
+  refresh = () => {
+    const now = Date.now();
+    const dTime = (now - this.lastRefreshTime) / 1000.0;
+
+    this.update(dTime);
+    this.render();
+
+    this.lastRefreshTime = now;
+
+    const game = this;
+    requestAnimationFrame(() => {
+      game.refresh();
+    });
+  };
+
+  update = (dTime) => {
+    this.sinceLastSpawn += dTime;
+    this.sinceLastSpawn > 1 && this.spawn();
+  };
+
+  render = () => {
+    this.sprites.forEach((sprite) => sprite.render());
+  };
 }
 
 class Sprite {
@@ -27,9 +59,11 @@ class Sprite {
     this.width = options.width;
     this.height = options.height;
     this.image = options.image;
+    this.x = options.x;
+    this.y = options.y;
   }
 
-  render() {
-    this.context.drawImage(this.image, 70, 20, 150, 100);
-  }
+  render = () => {
+    this.context.drawImage(this.image, this.x, this.y, 50, 30);
+  };
 }
